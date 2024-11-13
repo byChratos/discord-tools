9ba9ee84e34a962e16b382f23eb16b76707ef731
byChratos <byChratos@gmx.de>
2024-11-13 15:53:38 +0100

    init

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,12 @@
+
+function NavigationBar() {
+    
+    return(
+        <div className="h-full w-20 flex flex-col bg-red-600">
+
+        </div>
+    );
+
+}
+
+export default NavigationBar;
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from "../components/NavigationBar";
+
+function Layout() {
+    
+    return(
+        <div className="w-full h-full flex flex-row bg-blue-500">
+            <NavigationBar />
+        </div>
+    );
+}
+
+export default Layout;
