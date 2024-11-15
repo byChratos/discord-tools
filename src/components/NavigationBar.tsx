9980918121952e8acf24b39624f2839b2501e829
byChratos <byChratos@gmx.de>
2024-11-15 15:11:32 +0100

    Navigation Bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,9 +1,17 @@
+import React from "react";
+import NavigationButton from "./NavigationButton";
 
 function NavigationBar() {
     
+    const pages = [{ name: "H", link: "/"}, { name: "T", link: "/timestamp" }]
+
     return(
-        <div className="h-auto w-20 m-6 p-3 flex flex-col bg-red-600 rounded-[32px] items-center">
-            <p>Hallo</p>
+        <div className="h-fit w-20 m-4 p-2 flex flex-col bg-red-600 rounded-[32px] items-center self-center">
+            {pages.map((page, index) => (
+                <React.Fragment key={page.name}>
+                    <NavigationButton name={page.name} link={page.link} className={index != 0 ? 'mt-2' : ''}/>
+                </React.Fragment>
+            ))}
         </div>
     );
 
---
+++ src/components/NavigationButton.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+
+function NavigationButton({ name, link, className }: { name: string, link: string, className?: string }) {
+
+    const navigate = useNavigate();
+
+    return(
+        <button onClick={() => navigate(link)} className={twMerge(className, "w-full aspect-square bg-green-400 rounded-[24px]")}>
+            {name}
+        </button>
+    )
+
+}
+
+export default NavigationButton;
