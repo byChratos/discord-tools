390a165793eb8ad7001eb3fed2e246250d2373ae
byChratos <byChratos@gmx.de>
2025-02-02 21:01:02 +0100

    a

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -96,7 +96,7 @@ fn generate_timestamp(time: Time, date: Date, mode: Mode) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        //.plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
@@ -106,6 +106,8 @@ pub fn run() {
                         .build(),
                 )?;
             }
+            #[cfg(desktop)]
+            let _ = app.handle().plugin(tauri_plugin_updater::Builder::new().build());
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
